dc9fa407c76adc35335a8db9d4ce9995adcd3ceb
Thomas Petermann <thomas.petermann@eduvaud.ch>
2024-02-29 08:51:07 +0100

    feat: add Camera script

---
+++ Geometry_dash_like/Assets/Scripts/Camera.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+
+    [SerializeField] private Transform _player;
+    private Vector3 _offset = new Vector3(5, 0, -10);
+    private float _smoothTime = 0.25f;
+    private Vector3 _currentVelocity;
+
+    private float _startFollowingPosition = -5.25f;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //todo rempalcer "_startFollowingPosition" par la position de la camera
+        if (_player.position.x >= _startFollowingPosition)
+        {
+            float yPosition = 0;
+
+            if (_player.position.y >= (transform.position.y + 3))
+            {
+                yPosition = (transform.position.y + 2);
+            }
+
+            if (_player.position.y <= (transform.position.y - 5))
+            {
+                yPosition = (transform.position.y - 2);
+            }
+
+            FollowPlayer(yPosition);
+        }
+
+    }
+
+    private void FollowPlayer(float yPosition)
+    {
+        transform.position = Vector3.SmoothDamp(
+            transform.position,
+            new Vector3(_player.position.x, yPosition, _player.position.z) + _offset,
+            ref _currentVelocity,
+            _smoothTime
+        );
+
+    }
+}
