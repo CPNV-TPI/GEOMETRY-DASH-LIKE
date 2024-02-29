be0ab304e1b10cc3dd033ba721ae2830ed6fc01c
Thomas Petermann <thomas.petermann@eduvaud.ch>
2024-02-29 08:58:25 +0100

    feat: add CubeJump script

---
+++ Geometry_dash_like/Assets/Scripts/CubeJump.cs
@@ -0,0 +1,30 @@
+
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class CubeJump : MonoBehaviour
+{
+    private float _jumpForce = 10f; 
+    Rigidbody2D _rigidbody2D;
+    
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    
+    public void Jump()
+    {
+
+        if (IsGrounded())
+        {
+            _rigidbody2D.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return _rigidbody2D.velocity.y == 0;
+    }
+
+
+}
