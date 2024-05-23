4f3bd288a08be24623c77f932daff10af34157f7
Thomas Petermann <thomas.petermann@eduvaud.ch>
2024-05-23 09:54:51 +0200

    feat: add pause before player respawn

---
+++ Geometry_dash_like/Assets/Scripts/PlayerMovementManager.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+
+public class PlayerMovementManager : MonoBehaviour
+{
+    private ForwardMovement _forwardMovement;
+    private Rigidbody2D _rigidBody2D;
+    private PlayerController _playerController;
+
+    private void Awake()
+    {
+        _rigidBody2D = GetComponent<Rigidbody2D>();
+        _forwardMovement = GetComponent<ForwardMovement>();
+        _playerController = GetComponent<PlayerController>();
+    }
+
+    public void DisableMovement() {
+        _rigidBody2D.simulated = false;
+        _forwardMovement.enabled = false;
+        _playerController.enabled = false;
+    } 
+    
+    public void EnableMovement() {
+        _rigidBody2D.simulated = true;
+        _forwardMovement.enabled = true;
+        _playerController.enabled = true;
+    }
+}
+
+
