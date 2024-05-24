6ab25289e9aa814feb99f0dce3d98d0b50a8b8f6
Thomas Petermann <thomas.petermann@eduvaud.ch>
2024-05-24 11:05:37 +0200

    refactor: ScoreBoardUI class

--- Geometry_dash_like/Assets/Scripts/ScoreBoardUI.cs
+++ Geometry_dash_like/Assets/Scripts/ScoreBoardUI.cs
@@ -4,25 +4,22 @@ using UnityEngine.UI;
 public class ScoreBoardUI : MonoBehaviour
 {
     public GameObject scorePrefab;
-    
+
     public void InitializeScores(string levelIndex)
     {
         var dataList = ScoreManager.LoadScoreFromJson();
         var panel = transform.Find("Background/Scroll/Panel");
-        
-        foreach (Transform child in panel)
-        {
-            Destroy(child.gameObject);
-        }
-        
+
+        foreach (Transform child in panel) Destroy(child.gameObject);
+
         foreach (var scoreEntry in dataList.scoreEntryList)
         {
             if (scoreEntry.levelIndex != levelIndex) continue;
             var position = new Vector3(0, 0, 0);
             var score = Instantiate(scorePrefab, position, Quaternion.identity);
-            
+
             score.transform.SetParent(panel, false);
-            
+
             score.transform.Find("PlayerName").GetComponent<Text>().text = scoreEntry.playerName;
             score.transform.Find("Score").GetComponent<Text>().text = scoreEntry.score;
         }
