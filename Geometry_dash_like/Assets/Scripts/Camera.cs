e1526b0226ac0dce30d125a928b0b1130870c9ae
Thomas Petermann <thomas.petermann@eduvaud.ch>
2024-05-24 08:38:10 +0200

    fix: camera infinite error loop when player was destroyed

--- Geometry_dash_like/Assets/Scripts/Camera.cs
+++ Geometry_dash_like/Assets/Scripts/Camera.cs
@@ -2,37 +2,21 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
-
     [SerializeField] private Transform _player;
-    private Vector3 _offset = new Vector3(5, 0, -10);
-    private float _smoothTime = 0.25f;
     private Vector3 _currentVelocity;
+    private readonly Vector3 _offset = new(5, 0, -10);
+    private readonly float _smoothTime = 0.25f;
 
-    private float _startFollowingPosition = -5.25f;
+    private readonly float _startFollowingPosition = -5.25f;
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (_player.position.x >= _startFollowingPosition)
-        {
-            float yPosition = 0;
-
-            if (_player.position.y >= (transform.position.y + 3))
-            {
-                yPosition = (transform.position.y + 2);
-            }
-
-            if (_player.position.y <= (transform.position.y - 5))
-            {
-                yPosition = (transform.position.y - 2);
-            }
-
-            FollowPlayer(yPosition);
-        }
-
+        if (!_player) return;
+        FollowPlayer();
     }
 
-    private void FollowPlayer(float yPosition)
+    private void FollowPlayerX(float yPosition)
     {
         transform.position = Vector3.SmoothDamp(
             transform.position,
@@ -40,6 +24,22 @@ public class Camera : MonoBehaviour
             ref _currentVelocity,
             _smoothTime
         );
+    }
 
+    private void FollowPlayerY(float yPosition)
+    {
+        if (_player.position.x >= _startFollowingPosition)
+        {
+            if (_player.position.y >= transform.position.y + 3) yPosition = transform.position.y + 2;
+
+            if (_player.position.y <= transform.position.y - 5) yPosition = transform.position.y - 2;
+
+            FollowPlayerX(yPosition);
+        }
+    }
+
+    private void FollowPlayer()
+    {
+        FollowPlayerY(0);
     }
 }
