3075f279715f87a57e556371c2cbeab546384092
Thomas Petermann <thomas.petermann@eduvaud.ch>
2024-05-24 10:49:18 +0200

    refactor: ScoreManager class

--- Geometry_dash_like/Assets/Scripts/ScoreManager.cs
+++ Geometry_dash_like/Assets/Scripts/ScoreManager.cs
@@ -1,12 +1,11 @@
-using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
 public class ScoreManager
 {
-    private readonly ScoreEntry _scoreData;
     private const string FileName = "score.json";
-    
+    private readonly ScoreEntry _scoreData;
+
     public ScoreManager(string playerName, string score, string levelIndex)
     {
         _scoreData = new ScoreEntry
@@ -16,34 +15,26 @@ public class ScoreManager
             levelIndex = levelIndex
         };
     }
-    
+
     public void SaveScoreToJson()
     {
         var dataList = LoadScoreFromJson();
-        
+
         dataList.scoreEntryList.Add(_scoreData);
-        
+
         var json = JsonUtility.ToJson(dataList, true);
         var path = Path.Combine(Application.persistentDataPath, FileName);
         File.WriteAllText(path, json);
-
-        Debug.Log("Data saved to: " + path);
     }
-    
+
     public static ScoreEntryList LoadScoreFromJson()
     {
         var path = Path.Combine(Application.persistentDataPath, FileName);
-        if (File.Exists(path))
-        {
-            var json = File.ReadAllText(path);
-            return JsonUtility.FromJson<ScoreEntryList>(json);
-        }
-        else
-        {
-            return new ScoreEntryList();
-        }
+        if (!File.Exists(path)) return new ScoreEntryList();
+        var json = File.ReadAllText(path);
+        return JsonUtility.FromJson<ScoreEntryList>(json);
     }
-    
+
     private static string VerifyPlayerName(string playerName)
     {
         return playerName != "" ? playerName : "User";
